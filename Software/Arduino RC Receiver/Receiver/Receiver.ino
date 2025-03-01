89c3cc22d9963ffaadbda93601e0beab2b38301f
Milos Rasic <milosrasic98@gmail.com>
2025-03-01 19:53:50 +0100

    Adding Software files for the drone

---
+++ Software/Arduino RC Receiver/Receiver/Receiver.ino
@@ -0,0 +1,186 @@
+// RECEIVER
+
+/*
+ * This codes reads all of the channels from out transmitter and packs them into our PPM signal
+ * For testing purposes, we can add Serial prints for all of the values to check if everything is working correctly
+ * The roll on my transmitter is broken (bad potentiometer), so I've programmed the receiever to ignore that channel
+ * unless a specific AUX is activated
+ */
+
+/* 
+ *  This code is a modified version of the code from Electronoobs.com
+ *  Check out his tutorial on the Arduino Drone
+ */
+
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+////////////////////// PPM CONFIGURATION//////////////////////////
+#define channel_number 6  //set the number of channels
+#define sigPin 2  //set PPM signal output pin on the arduino
+#define PPM_FrLen 27000  //set the PPM frame length in microseconds (1ms = 1000µs)
+#define PPM_PulseLen 400  //set the pulse length
+//////////////////////////////////////////////////////////////////
+
+int ppm[channel_number];
+
+const uint64_t pipeIn =  0xE8E8F0F0E1LL;
+
+RF24 radio(7, 8);
+
+// The sizeof this struct should not exceed 32 bytes
+struct MyData1 {
+  byte throttle;
+  byte yaw;
+  byte pitch;
+  byte roll;
+  byte AUX1;
+  byte AUX2;
+  byte AUX3;
+  byte AUX4;
+  byte AUX5;
+  byte AUX6;
+  byte AUX7;
+  byte gimbalY;
+  byte gimbalZ;
+};
+
+MyData1 data;
+
+void resetData() 
+{
+  // 'safe' values to use when no radio input is detected
+  data.throttle = 0;
+  data.yaw = 127;
+  data.pitch = 127;
+  data.roll = 127;
+  data.AUX1 = 0;
+  data.AUX2= 0;
+  data.AUX3 = 0;
+  data.AUX4 = 0;
+  data.AUX5 = 0;
+  data.AUX6 = 0;
+  data.AUX7 = 0;
+  data.gimbalY = 127;
+  data.gimbalZ = 127;
+  
+  setPPMValuesFromData();
+}
+
+void setPPMValuesFromData()
+{
+  ppm[0] = map(data.throttle, 0, 255, 1000, 2000);
+  ppm[1] = map(data.yaw, 0, 255, 1000, 2000);
+  ppm[2] = map(data.pitch,    0, 255, 1000, 2000);
+
+  
+        
+        if(data.AUX3 == 1){
+          ppm[3] = 1500;
+        }
+        else{
+          ppm[3] = map(data.roll,     0, 255, 1000, 2000);
+        } 
+      
+  
+  
+  ppm[4] = map(data.AUX1,     0, 1, 1000, 2000);
+  ppm[5] = map(data.AUX2,     0, 1, 1000, 2000);  
+  }
+
+/**************************************************/
+
+void setupPPM() {
+  pinMode(sigPin, OUTPUT);
+  digitalWrite(sigPin, 0);  //set the PPM signal pin to the default state (off)
+
+  cli();
+  TCCR1A = 0; // set entire TCCR1 register to 0
+  TCCR1B = 0;
+
+  OCR1A = 100;  // compare match register (not very important, sets the timeout for the first interrupt)
+  TCCR1B |= (1 << WGM12);  // turn on CTC mode
+  TCCR1B |= (1 << CS11);  // 8 prescaler: 0,5 microseconds at 16mhz
+  TIMSK1 |= (1 << OCIE1A); // enable timer compare interrupt
+  sei();
+}
+
+void setup()
+{  
+  resetData();
+  setupPPM();
+  
+  // Set up radio module
+  radio.begin();
+  radio.setDataRate(RF24_250KBPS); // Both endpoints must have this set the same
+  radio.setAutoAck(false);
+
+  radio.openReadingPipe(1,pipeIn);
+  radio.startListening();
+}
+
+/**************************************************/
+
+unsigned long lastRecvTime = 0;
+
+void recvData()
+{  
+  while ( radio.available() ) {        
+    radio.read(&data, sizeof(MyData1));
+    lastRecvTime = millis();
+  }
+}
+
+/**************************************************/
+
+void loop()
+{
+  recvData();
+
+  unsigned long now = millis();
+  if ( now - lastRecvTime > 1000 ) {
+    // signal lost?
+    resetData();
+  }
+  
+  setPPMValuesFromData();
+}
+
+/**************************************************/
+
+//#error Delete this line befor you cahnge the value (clockMultiplier) below
+#define clockMultiplier 2 // set this to 2 if you are using a 16MHz arduino, leave as 1 for an 8MHz arduino
+
+ISR(TIMER1_COMPA_vect){
+  static boolean state = true;
+
+  TCNT1 = 0;
+
+  if ( state ) {
+    //end pulse
+    PORTD = PORTD & ~B00000100; // turn pin 2 off. Could also use: digitalWrite(sigPin,0)
+    OCR1A = PPM_PulseLen * clockMultiplier;
+    state = false;
+  }
+  else {
+    //start pulse
+    static byte cur_chan_numb;
+    static unsigned int calc_rest;
+
+    PORTD = PORTD | B00000100; // turn pin 2 on. Could also use: digitalWrite(sigPin,1)
+    state = true;
+
+    if(cur_chan_numb >= channel_number) {
+      cur_chan_numb = 0;
+      calc_rest += PPM_PulseLen;
+      OCR1A = (PPM_FrLen - calc_rest) * clockMultiplier;
+      calc_rest = 0;
+    }
+    else {
+      OCR1A = (ppm[cur_chan_numb] - PPM_PulseLen) * clockMultiplier;
+      calc_rest += ppm[cur_chan_numb];
+      cur_chan_numb++;
+    }     
+  }
+}
---
+++ Software/Arduino RC Transmitter/Transmitter/Transmitter.ino
@@ -0,0 +1,121 @@
+// TRANSMITTER CODE
+
+/* 
+ *  This code will be used for sending data using the NRF24 to the Arduino on our drone
+ *  
+ *  Connections for this transmitter are:
+ *  NRF24 - standard SPI connection and the other 2 pins to pins 7 & 8
+ *  All of the switches are connected to digital pins, they are wires up so they pull the input to ground,
+ *  this is so we can use an internall pullup on Arduino pins
+ *  On the analog pins we have all of the joysticks from our transmitter
+ *  A4 and A5 are left unconnected so we can have I2C devices further on if we desire
+ *  
+ */
+ 
+
+/* 
+ *  This code is a modified version of the code from Electronoobs.com
+ *  Check out his tutorial on the Arduino Drone
+ *  
+ */
+ 
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+#define Pin_AUX1 2
+#define Pin_AUX2 3
+#define Pin_AUX3 4
+#define Pin_AUX4 5
+#define Pin_AUX5 6
+#define Pin_AUX6 9
+#define Pin_AUX7 10
+
+/*Create a unique pipe out. The receiver has to 
+  wear the same unique code*/
+  
+const uint64_t pipeOut = 0xE8E8F0F0E1LL;
+
+RF24 radio(7, 8); // select  CSN  pin
+
+// The sizeof this struct should not exceed 32 bytes
+// This gives us up to 32 8 bits channals
+struct MyData {
+  byte throttle;
+  byte yaw;
+  byte pitch;
+  byte roll;
+  byte AUX1;
+  byte AUX2;
+  byte AUX3;
+  byte AUX4;
+  byte AUX5;
+  byte AUX6;
+  byte AUX7;
+  byte gimbalY;
+  byte gimbalZ;
+};
+
+MyData data;
+
+void resetData() 
+{
+  data.throttle = 0;
+  data.yaw = 127;
+  data.pitch = 127;
+  data.roll = 127;
+  data.AUX1 = 0;
+  data.AUX2 = 0;
+  data.AUX3 = 0;
+  data.AUX4 = 0;
+  data.AUX5 = 0;
+  data.AUX6 = 0;
+  data.AUX7 = 0;
+  data.gimbalY = 0;
+  data.gimbalZ = 0;
+}
+
+void setup()
+{
+  //Start everything up
+  pinMode(Pin_AUX1, INPUT_PULLUP);
+  pinMode(Pin_AUX2, INPUT_PULLUP);
+  pinMode(Pin_AUX3, INPUT_PULLUP);
+  pinMode(Pin_AUX4, INPUT_PULLUP);
+  pinMode(Pin_AUX5, INPUT_PULLUP);
+  pinMode(Pin_AUX6, INPUT_PULLUP);
+  pinMode(Pin_AUX7, INPUT_PULLUP);
+  radio.begin();
+  radio.setAutoAck(false);
+  radio.setDataRate(RF24_250KBPS);
+  radio.openWritingPipe(pipeOut);
+  resetData();
+}
+
+int mapJoystickValues(int val, int lower, int middle, int upper, bool reverse)
+{
+  val = constrain(val, lower, upper);
+  if ( val < middle )
+    val = map(val, lower, middle, 0, 128);
+  else
+    val = map(val, middle, upper, 128, 255);
+  return ( reverse ? 255 - val : val );
+}
+
+void loop()
+{
+  data.throttle = mapJoystickValues( analogRead(A0), 60, 490, 925, true );
+  data.yaw      = mapJoystickValues( analogRead(A1),  0, 520, 956, false );
+  data.pitch    = mapJoystickValues( analogRead(A2), 44, 502, 934, true );
+  data.roll     = mapJoystickValues( analogRead(A3), 31, 320, 608, false );
+  data.gimbalY  = map(analogRead(A6), 0, 1023, 0, 255);
+  data.gimbalZ  = map(analogRead(A7), 0, 1023, 0, 255);
+  data.AUX1     = digitalRead(Pin_AUX1);
+  data.AUX2     = digitalRead(Pin_AUX2);
+  data.AUX3     = digitalRead(Pin_AUX3);
+  data.AUX4     = digitalRead(Pin_AUX4);
+  data.AUX5     = digitalRead(Pin_AUX5);
+  data.AUX6     = digitalRead(Pin_AUX6);
+  data.AUX7     = digitalRead(Pin_AUX7);
+  radio.write(&data, sizeof(MyData));
+}
